7152688cf9d4ebe41171315f14958c07a5429f21
Dennis Borrmann <debbo@hotmail.de>
2026-01-04 11:23:27 +0100

    Update 20260104014713_create_sound_keywords.exs

--- priv/repo/migrations/20260104014713_create_sound_keywords.exs
+++ priv/repo/migrations/20260104014713_create_sound_keywords.exs
@@ -2,5 +2,15 @@ defmodule Soundboard.Repo.Migrations.CreateSoundKeywords do
   use Ecto.Migration
 
   def change do
+    create table(:sound_keywords) do
+      add :keyword, :string, null: false
+      add :sound_id, references(:sounds, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create index(:sound_keywords, [:sound_id])
+    create index(:sound_keywords, [:keyword])
+    create unique_index(:sound_keywords, [:sound_id, :keyword])
   end
 end
